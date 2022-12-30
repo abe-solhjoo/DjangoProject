cd5b76214233c5bf45efa4f7d1427669aa4325ad
Abe_soo <eb.solhjoo@gmail.com>
2022-12-30 16:25:22 +0330

    Add Http 404 for product

--- products/models.py
+++ products/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 
+class ProductManager(models.Manager):
+    def get_by_id(self, productId):
+        qs = self.get_queryset().filter(id=productId)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
+
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2, default=0.0)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Product
+from django.http import Http404
 
 
 # Create your views here.
@@ -19,3 +20,17 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail_list.html"
+
+    # def get_context_data(self,*args, object_list=None, **kwargs):
+    #     context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+    #
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        productId = self.kwargs.get('pk')
+        product = Product.objects.get_by_id(productId)
+        if product is None:
+            raise Http404('Product dose not found from custom model manager')
+        return product
